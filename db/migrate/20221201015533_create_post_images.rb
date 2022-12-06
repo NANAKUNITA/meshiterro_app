d7bc0bf0747ed8c3694eaa80df373bbd6095830b
NANAKUNITA <116545439+NANAKUNITA@users.noreply.github.com>
2022-12-06 19:16:16 +0900

    Update 20221201015533_create_post_images.rb

--- db/migrate/20221201015533_create_post_images.rb
+++ db/migrate/20221201015533_create_post_images.rb
@@ -1,11 +1,7 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-      t.string :shop_name
-      t.text :image_id
-      t.text :caption
-      t.integer :user_id
-
+     
       t.timestamps
     end
   end
